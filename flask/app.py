f43086c0278ef5d7971f3942df0d415eec0c2dfc
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-10-15 22:03:07 -0300

    :sparkles: Adicionado flask_session

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_socketio import SocketIO, emit
+from flask_session import Session
 
 app = Flask(__name__)
 io = SocketIO(app)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SECRET_KEY'] = 'triviagame'
 
 @app.route("/")
 def hello_word():
   return "<p>Hello World</p>"
+
+@io.on('join')
+def join(message):
+  print(message)
 
 @io.on('sendChatMessage')
 def send_chat_message_handler(msg):
