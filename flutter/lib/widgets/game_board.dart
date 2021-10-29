bfda64c9634e090958d869051ee60e350ce9ec5c
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2021-10-29 19:43:13 -0300

    :lipstick: Estilizando campo onde aparecerá a palavra da rodada.

--- flutter/lib/widgets/game_board.dart
+++ flutter/lib/widgets/game_board.dart
@@ -5,7 +5,55 @@ import 'package:hexcolor/hexcolor.dart';
 //  teste.split('').map((element) => Text(element)).toList(),
 class GameBoard extends StatelessWidget {
   GameBoard({Key? key}) : super(key: key);
-  String teste = "lalaladawdwadwdwawd";
+  String teste = "Sá*ado *e So*";
+
+  renderLetter(letter) {
+    var caracter = letter;
+
+    if (letter == '*') {
+      caracter = ' ';
+    } else if (letter == ' ') {
+      caracter = '';
+    }
+
+    return Text(
+      caracter,
+      textAlign: TextAlign.center,
+      style: const TextStyle(
+          color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
+    );
+  }
+
+  decideLetterBackground(letter) {
+    if (letter == '*') {
+      return HexColor('#393B4B');
+    } else if (letter == ' ') {
+      return null;
+    }
+
+    return HexColor('#6B5FCD');
+  }
+
+  decideShadow(letter) {
+    if (letter == ' ') {
+      return const BoxShadow(color: Colors.transparent);
+    }
+
+    var color = HexColor('#877CE8');
+    if (letter == '*') {
+      color = HexColor('#4F5164');
+    }
+
+    // return HexColor('#877CE8');
+
+    return BoxShadow(
+      color: color,
+      spreadRadius: 1,
+      blurRadius: 0,
+      offset: const Offset(1, 1.5), // changes position of shadow
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // debugPrint(players.length);
@@ -22,7 +70,7 @@ class GameBoard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("TEXTO DE ${teste.length} LETRAS."),
+                  Text("TEXTO DE ${teste.length} LETRAS"),
                   Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
@@ -30,18 +78,22 @@ class GameBoard extends StatelessWidget {
                         Expanded(
                             child: Wrap(
                           alignment: WrapAlignment.center,
-                          spacing: 8.0, // gap between adjacent chips
-                          runSpacing: 4.0, // gap between lines
+                          spacing: 0, // gap between adjacent chips
+                          runSpacing: 4, // gap between lines
                           children: teste
                               .split('')
                               .map((element) => Container(
-                                  margin: const EdgeInsets.all(10.0),
-                                  color: Colors.amber[600],
-                                  width: 20,
-                                  height: 20,
-                                  child: Text(
-                                    element,
-                                  )))
+                                  margin: const EdgeInsets.all(3),
+                                  alignment: Alignment.center,
+                                  width: 25,
+                                  height: 25,
+                                  decoration: BoxDecoration(
+                                    color: decideLetterBackground(element),
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(2)),
+                                    boxShadow: [decideShadow(element)],
+                                  ),
+                                  child: renderLetter(element)))
                               .toList(),
                         ))
                       ]),
