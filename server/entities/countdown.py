1ba6a4c5ba99fd980975315e3e3cb8fa92d176b0
JessePires <jesserocha@alunos.utfpr.edu.br>
2021-11-08 19:36:01 -0300

    :sparkles: Invertendo timer.

--- server/entities/countdown.py
+++ server/entities/countdown.py
@@ -2,16 +2,15 @@ class CountDown():
   
   def __init__(self, time, sio, event, room):
     self.started = False
-    self.time = time
     self.event = event
-    self.counter = 0
+    self.counter = time
     self.room = room
     self.sio = sio
     
   def start(self):
-    while self.counter <= self.time and self.started == True:
+    while self.counter >= 0 and self.started == True:
       self.sio.emit(self.event, self.counter, to=self.room)
-      self.counter += 1
+      self.counter -= 1
       self.sio.sleep(1)
 
   def stop(self):
